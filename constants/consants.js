09f367c1b72c663a4ad244542f0c29c82c8490bd
Desmond Nyamador <dselasi4u@gmail.com>
2021-10-03 15:07:39 +0000

    Devcycle section

--- constants/consants.js
+++ constants/consants.js
@@ -1,120 +1,161 @@
-const TECH_STACK_ROUTE = '/svg/techStack/';
+const TECH_STACK_ROUTE = "/svg/techStack/";
 export const techStackImages = [
-	{
-		url: `${TECH_STACK_ROUTE}html5.svg`,
-		id: 1,
-	},
-	{
-		url: `${TECH_STACK_ROUTE}css.svg`,
-		id: 2,
-	},
-	{
-		url: `${TECH_STACK_ROUTE}javascript.svg`,
-		id: 3,
-	},
-	{
-		url: `${TECH_STACK_ROUTE}react.svg`,
-		id: 4,
-	},
-	{
-		url: `${TECH_STACK_ROUTE}vue.svg`,
-		id: 5,
-	},
-	{
-		url: `${TECH_STACK_ROUTE}angular.svg`,
-		id: 6,
-	},
-	{
-		url: `${TECH_STACK_ROUTE}nodejs.svg`,
-		id: 7,
-	},
-	{
-		url: `${TECH_STACK_ROUTE}python.svg`,
-		id: 8,
-	},
-	{
-		url: `${TECH_STACK_ROUTE}docker.svg`,
-		id: 9,
-	},
-	{
-		url: `${TECH_STACK_ROUTE}google-cloud.svg`,
-		id: 10,
-	},
-	{
-		url: `${TECH_STACK_ROUTE}aws.svg`,
-		id: 11,
-	},
+  {
+    url: `${TECH_STACK_ROUTE}html5.svg`,
+    id: 1,
+  },
+  {
+    url: `${TECH_STACK_ROUTE}css.svg`,
+    id: 2,
+  },
+  {
+    url: `${TECH_STACK_ROUTE}javascript.svg`,
+    id: 3,
+  },
+  {
+    url: `${TECH_STACK_ROUTE}react.svg`,
+    id: 4,
+  },
+  {
+    url: `${TECH_STACK_ROUTE}vue.svg`,
+    id: 5,
+  },
+  {
+    url: `${TECH_STACK_ROUTE}angular.svg`,
+    id: 6,
+  },
+  {
+    url: `${TECH_STACK_ROUTE}nodejs.svg`,
+    id: 7,
+  },
+  {
+    url: `${TECH_STACK_ROUTE}python.svg`,
+    id: 8,
+  },
+  {
+    url: `${TECH_STACK_ROUTE}docker.svg`,
+    id: 9,
+  },
+  {
+    url: `${TECH_STACK_ROUTE}google-cloud.svg`,
+    id: 10,
+  },
+  {
+    url: `${TECH_STACK_ROUTE}aws.svg`,
+    id: 11,
+  },
 ];
 
-const INDUSTRIES_ROUTE = '/svg/industries/';
+const INDUSTRIES_ROUTE = "/svg/industries/";
 export const industryImages = [
-	{
-		url: `${INDUSTRIES_ROUTE}hospital.svg`,
-		id: 1,
-		name: 'Healthcare',
-	},
-	{
-		url: `${INDUSTRIES_ROUTE}book.svg`,
-		id: 2,
-		name: 'Education',
-	},
-	{
-		url: `${INDUSTRIES_ROUTE}ship.svg`,
-		id: 3,
-		name: 'Logistics',
-	},
-	{
-		url: `${INDUSTRIES_ROUTE}package.svg`,
-		id: 4,
-		name: 'E-commerce',
-	},
-	{
-		url: `${INDUSTRIES_ROUTE}camera.svg`,
-		id: 5,
-		name: 'Media',
-	},
-	{
-		url: `${INDUSTRIES_ROUTE}store.svg`,
-		id: 6,
-		name: 'Retail',
-	},
-	{
-		url: `${INDUSTRIES_ROUTE}money-withdrawal.svg`,
-		id: 7,
-		name: 'Fintech',
-	},
-	{
-		url: `${INDUSTRIES_ROUTE}plane-departure.svg`,
-		id: 8,
-		name: 'Travel',
-	},
+  {
+    url: `${INDUSTRIES_ROUTE}hospital.svg`,
+    id: 1,
+    name: "Healthcare",
+  },
+  {
+    url: `${INDUSTRIES_ROUTE}book.svg`,
+    id: 2,
+    name: "Education",
+  },
+  {
+    url: `${INDUSTRIES_ROUTE}ship.svg`,
+    id: 3,
+    name: "Logistics",
+  },
+  {
+    url: `${INDUSTRIES_ROUTE}package.svg`,
+    id: 4,
+    name: "E-commerce",
+  },
+  {
+    url: `${INDUSTRIES_ROUTE}camera.svg`,
+    id: 5,
+    name: "Media",
+  },
+  {
+    url: `${INDUSTRIES_ROUTE}store.svg`,
+    id: 6,
+    name: "Retail",
+  },
+  {
+    url: `${INDUSTRIES_ROUTE}money-withdrawal.svg`,
+    id: 7,
+    name: "Fintech",
+  },
+  {
+    url: `${INDUSTRIES_ROUTE}plane-departure.svg`,
+    id: 8,
+    name: "Travel",
+  },
 ];
 
-const TEAMS_ROUTE = '/svg/team/';
+const TEAMS_ROUTE = "/svg/team/";
 export const teamImages = [
-	{
-		url: `${TEAMS_ROUTE}desmondaf.svg`,
-		id: 1,
-		name: 'Desmond Afedzie',
-	},
-	{
-		url: `${TEAMS_ROUTE}benson.svg`,
-		id: 2,
-		name: 'Damilola Randolph',
-	},
-	{
-		url: `${TEAMS_ROUTE}justin.svg`,
-		id: 3,
-		name: 'Justin  Amehunke',
-	},
-	{
-		url: `${TEAMS_ROUTE}leslie.svg`,
-		id: 4,
-		name: 'Leslie Wahiermeh',
-	},
-	{
-		url: `${TEAMS_ROUTE}desmond.svg`,
-		id: 5,
-		name: 'Desmond Nyamador',
-	},
+  {
+    url: `${TEAMS_ROUTE}desmondaf.svg`,
+    id: 1,
+    name: "Desmond Afedzie",
+  },
+  {
+    url: `${TEAMS_ROUTE}benson.svg`,
+    id: 2,
+    name: "Damilola Randolph",
+  },
+  {
+    url: `${TEAMS_ROUTE}justin.svg`,
+    id: 3,
+    name: "Justin  Amehunke",
+  },
+  {
+    url: `${TEAMS_ROUTE}leslie.svg`,
+    id: 4,
+    description: "Lorem ipsum dolor sit amet consecteur pur amit sin tupi.",
+    name: "Leslie Wahiermeh",
+  },
+  {
+    url: `${TEAMS_ROUTE}desmond.svg`,
+    id: 5,
+    name: "Desmond Nyamador",
+  },
+];
+
+const PROCESS_STAGE_ROUTE = "/svg/dev_process/";
+export const devProcessImages = [
+  {
+    url: `${PROCESS_STAGE_ROUTE}lightbulb-alt.svg`,
+    id: 1,
+    name: "Ideation",
+    description: "Lorem ipsum dolor sit amet consecteur pur amit sin tupi.",
+  },
+  {
+    url: `${PROCESS_STAGE_ROUTE}chart-line.svg`,
+    id: 2,
+    name: "Analysis",
+    description: "Lorem ipsum dolor sit amet consecteur pur amit sin tupi.",
+  },
+  {
+    url: `${PROCESS_STAGE_ROUTE}laptop-cloud.svg`,
+    id: 3,
+    name: "Prototyping",
+    description: "Lorem ipsum dolor sit amet consecteur pur amit sin tupi.",
+  },
+  {
+    url: `${PROCESS_STAGE_ROUTE}process.svg`,
+    id: 4,
+    name: "Systematize",
+    description: "Lorem ipsum dolor sit amet consecteur pur amit sin tupi.",
+  },
+  {
+    url: `${PROCESS_STAGE_ROUTE}code-branch.svg`,
+    id: 5,
+    name: "Implementation",
+    description: "Lorem ipsum dolor sit amet consecteur pur amit sin tupi.",
+  },
+  {
+    url: `${PROCESS_STAGE_ROUTE}redo.svg`,
+    id: 6,
+    name: "Re-iteration",
+    description: "Lorem ipsum dolor sit amet consecteur pur amit sin tupi.",
+  },
 ];
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,52 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
 
-import Layout from '../containers/Layout'
-import HeroSection from '../containers/HeroSection';
-import SideMenu from '../containers/SideMenu'
-import TechStack from '../containers/TechStack';
-import DevCycle from '../containers/DevCycle';
-import Industries from '../containers/Industries';
-import Team from '../containers/Team';
-import Testimonials from '../containers/Testimonials';
-import Footer from '../containers/Footer';
+import Layout from "../containers/Layout";
+import HeroSection from "../containers/HeroSection";
+import SideMenu from "../containers/SideMenu";
+import TechStack from "../containers/TechStack";
+import DevCycle from "../containers/DevCycle";
+import Industries from "../containers/Industries";
+import Team from "../containers/Team";
+import Testimonials from "../containers/Testimonials";
+import Footer from "../containers/Footer";
 
 export default function Home() {
-	const [menuOpened, setMenuOpened] = useState(false)
+  const [menuOpened, setMenuOpened] = useState(false);
 
-	useEffect(() => {
-		if (menuOpened) {
-			document.classList = "overflow-hidden"
-		} else {
-			document.classList = ""
-		}
-	}, [menuOpened])
+  useEffect(() => {
+    menuOpened
+      ? (document.classList = "overflow-hidden")
+      : (document.classList = "");
+  }, [menuOpened]);
 
+  return (
+    <div>
+      <Head>
+        <title>Octave Systems | Homepage </title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
 
-	return (
-		<div>
-			<Head>
-				<title>Octave Systems | Homepage </title>
-				<link rel="icon" href="/favicon.ico" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-				></link>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap"
-					rel="stylesheet"
-				></link>
-			</Head>
-
-			<Layout>
-				<HeroSection onMenuClick={() => menuOpened ? setMenuOpened(false) : setMenuOpened(true)}/>
-				<TechStack />
-				<DevCycle />
-				<Industries />
-				<Team />
-				<Testimonials />
-				<Footer />
-			</Layout>
-			<SideMenu opened={menuOpened} onClose={() => setMenuOpened(false)}/>
-		</div>
-	);
+      <Layout>
+        <HeroSection
+          onMenuClick={() =>
+            menuOpened ? setMenuOpened(false) : setMenuOpened(true)
+          }
+        />
+        <TechStack />
+        <DevCycle />
+        <Industries />
+        <Team />
+        <Testimonials />
+        <Footer />
+      </Layout>
+      <SideMenu opened={menuOpened} onClose={() => setMenuOpened(false)} />
+    </div>
+  );
 }
