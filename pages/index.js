f177631f811b23ae39b134948447a975a2cd76ca
Desmond Nyamador <dselasi4u@gmail.com>
2022-01-07 17:17:18 +0000

    Header layout

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,72 @@
 import Head from "next/head";
-import Image from "next/image";
+import * as Unicons from "@iconscout/react-unicons";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Octave Systems | Homepage </title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <title>Octave Systems </title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Lora:wght@700&family=Open+Sans:wght@400&display=swap"
           rel="stylesheet"
-        ></link>
+        />
       </Head>
 
-      <div className="flex flex-row items-center justify-center h-screen bg-black">
-        <div className="flex flex-col items-center">
-          <Image
-            src="/logo.svg"
-            alt="Octave Logo"
-            height="100px"
-            width="auto"
-          />
-          <p className="mt-10 text-white">
-            Octave Systems - Website under construction
-          </p>
+      <header className="h-screen p-4 text-white bg-black md:p-0">
+        <nav className="md:w-11/12 md:m-auto">
+          <p className="text-gray-100">navbar</p>
+        </nav>
+        <div className="md:w-11/12 md:m-auto">
+          <div className="flex flex-col w-full md:pt-20 md:flex-row">
+            <section className="md:w-1/2 md:mr-10">
+              <h1 className="mb-4 font-serif text-2xl md:text-4xl">
+                Building digital products, brands, and experiences
+              </h1>
+              <p>
+                We transform your business problems into beautiful creative
+                solutions. Let us help you tell your story
+              </p>
+              <button className="flex flex-row py-2 border-b border-white">
+                Connect with us <Unicons.UilArrowRight />
+              </button>
+
+              <div
+                className="w-full mt-10 mb-4 rounded-md md:mb-0 bg-gray-50"
+                style={{ height: "400px" }}
+              ></div>
+            </section>
+
+            <section className="md:w-1/2">
+              <div className="hidden md:h-1/2 md:block" />
+              <div className="md:flex md:flex-col md:h-1/2 md:justify-end">
+                <h2 className="mb-4 font-serif text-2xl md:text-4xl">
+                  We build...
+                </h2>
+                <ul>
+                  <li className="flex flex-row">
+                    <Unicons.UilArrowRight />
+                    Mobile Applications
+                  </li>
+                  <li className="flex flex-row">
+                    <Unicons.UilArrowRight />
+                    Web Applications
+                  </li>
+                  <li className="flex flex-row">
+                    <Unicons.UilArrowRight />
+                    User Interface Designs
+                  </li>
+                  <li className="flex flex-row">
+                    <Unicons.UilArrowRight />
+                    Websites
+                  </li>
+                </ul>
+              </div>
+            </section>
+          </div>
         </div>
-      </div>
+      </header>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,42 @@
 module.exports = {
-	purge: [],
-	darkMode: false, // or 'media' or 'class'
-	animation: true,
-	theme: {
-		extend: {
-			colors: {
-				purple: '#240127',
-				lightPurple: '#F9E5F9',
-			},
-			backgroundImage: (theme) => ({
-				'hero-pattern': "url('/svg/pattern.svg')",
-				'teams-pattern': "url('/svg/pattern_light.svg')",
-			}),
-			keyframes: {
-				animation: {
-					pul: 'bgpulse 2s ease-in-out infinite'
-				},
-				bgpulse: {
-					'0% 100%':{ 'background-color': '#f9c5d1',
-						'background-image': 'linear-gradient(315deg, #f9c5d1 0%, #9795ef 74%)'
-					},
-					'50%': {
-						'background-color': '#861657',
-						'background-image': 'linear-gradient(326deg, #861657 0%, #ffa69e 74%)'
-
-					}
-					
-				}
-			}
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  animation: true,
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Lora"],
+        body: ["Open Sans"],
+      },
+      colors: {
+        purple: "#240127",
+        lightPurple: "#F9E5F9",
+      },
+      backgroundImage: (theme) => ({
+        "hero-pattern": "url('/svg/pattern.svg')",
+        "teams-pattern": "url('/svg/pattern_light.svg')",
+      }),
+      keyframes: {
+        animation: {
+          pul: "bgpulse 2s ease-in-out infinite",
+        },
+        bgpulse: {
+          "0% 100%": {
+            "background-color": "#f9c5d1",
+            "background-image":
+              "linear-gradient(315deg, #f9c5d1 0%, #9795ef 74%)",
+          },
+          "50%": {
+            "background-color": "#861657",
+            "background-image":
+              "linear-gradient(326deg, #861657 0%, #ffa69e 74%)",
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
